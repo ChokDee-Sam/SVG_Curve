c74a9640c0dbee0503cc4a0f312b6821b9a0ba73
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-11-06 22:41:56 +0700

    🚧 JS > part 1

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = document.querySelector(".path")
+const menuToggle = document.querySelector(".menu-tog")
+const ul = document.querySelector("ul")
+// console.log(path.getAttribute("d"))
+
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t
+}
+
+let toggle = false
+
+// Start SVG at bottom of screen (menu is not active)
+let y = 100
+let c = 100
+
+function animate() {
+  if (toggle) {
+    y = lerp(y, 0, 0.065).toFixed(2)
+    c = lerp(y, 0, 0.065).toFixed(2)
+    path.setAttribute(
+      "d",
+      `M 0 ${y} L 0 100 100 100 100 ${y} C 50 ${c} 50 ${c}, 0 ${y}`
+    )
+  } else {
+    y = lerp(y, 100, 0.065).toFixed(2)
+    c = lerp(y, 100, 0.065).toFixed(2)
+    path.setAttribute(
+      "d",
+      `M 0 ${y} L 0 100 100 100 100 ${y} C 50 ${c} 50 ${c}, 0 ${y}`
+    )
+  }
+  requestAnimationFrame(animate)
+}
+
+
